adbeaa1cc4dff8a8258a46e3878b348b15841c78
R. Abdurrohman <rifaiabdurrohman8@gmail.com>
2024-05-04 10:20:03 +0700

    aggregate

---
+++ scripts/11-aggregate.js
@@ -0,0 +1,4 @@
+// fungsi dari aggregate adalah menggabungkan dari beberapa modul!
+export { Person } from "./4-class.js";
+export { sayHello, sayGoodBye } from "./2-say.js";
+export { sum } from "./5-multiple.js";

76f99a9126c680268df085c4f9e11186f27508d7
R. Abdurrohman <rifaiabdurrohman8@gmail.com>
2024-05-04 10:01:16 +0700

    module object

---
+++ scripts/10-module-object.js
@@ -0,0 +1,13 @@
+export const firstName = "Rifai";
+export const middleName = "Abdurrohman";
+export const lastName = "Tyas";
+
+export function hello() {
+  console.info("Hello from Tyas");
+}
+
+export class Person {
+  constructor() {
+    this.name = "Tyas";
+  }
+}
